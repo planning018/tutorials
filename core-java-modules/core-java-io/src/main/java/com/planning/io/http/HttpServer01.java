57b8ef278ac578623a7071f10647ebccfc4d01b3
planning <13057588636@163.com>
2022-04-02 15:07:51 +0800

    NIO tutorials

---
+++ core-java-modules/core-java-io/src/main/java/com/planning/io/http/HttpServer01.java
@@ -0,0 +1,44 @@
+package com.planning.io.http;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 单线程的 socket 程序
+ *
+ * @author yxc
+ * @date 2022/4/1 6:17 下午
+ */
+public class HttpServer01 {
+
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket = new ServerSocket(8801);
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                service(socket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void service(Socket socket) {
+        try {
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body = "hello,nio1";
+            printWriter.println("Content-Length:" + body.getBytes().length);
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.flush();
+            printWriter.close();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ core-java-modules/core-java-io/src/main/java/com/planning/io/http/netty/HttpInitializer.java
@@ -0,0 +1,23 @@
+package com.planning.io.http.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * @author yxc
+ * @date 2022/4/1 6:23 下午
+ */
+public class HttpInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    public void initChannel(SocketChannel ch) {
+        ChannelPipeline p = ch.pipeline();
+        p.addLast(new HttpServerCodec());
+        //p.addLast(new HttpServerExpectContinueHandler());
+        p.addLast(new HttpObjectAggregator(1024 * 1024));
+        p.addLast(new HttpHandler());
+    }
+}
